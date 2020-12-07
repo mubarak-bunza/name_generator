70ba6599b6228035a99a70fa23dcd31e7541d2d7
Abubakar Bunza Mubarak <mubarakabubakar227@gmail.com>
2020-12-07 22:41:42 +0000

    handled number of vowels and consonants exceptions

--- main.py
+++ main.py
@@ -1,46 +1,46 @@
 import random
 
 def is_vowel(letters):
-  if len(letters) >= 3:
-    vowels = 'aioue'
-    result = set()
-    for i in range(len(letters)):
-      result.add(letters[i].lower() in vowels)
-    
-    if False not in result:
-      return True
-    else:
-      return False
+  vowels = 'aioue'
+  result = set()
+  for i in range(len(letters)):
+    result.add(letters[i].lower() in vowels)
+  
+  if False not in result:
+    return True
   else:
-    print('Parameters can not be less than 3 characters')
+    return False
   
 
-v = is_vowel('aqioue')
-print(v)
+# v = is_vowel('qqqq')
+# print(v)
 
 
 def name_generator(vowels, consonant):
   fname = ''
   lname = ''
-  if is_vowel(vowels):
-    fname = ''.join(random.choice(vowels) for _ in vowels)
+  try:
+    assert len(vowels) >= 3 and len(consonant) >= 3
+  except:
+    return "Vowels and Consonants can not be less than 3 characters"
   else:
-    print('This param accepts vowels only')
-    return
+    if is_vowel(vowels):
+      fname = ''.join(random.choice(vowels) for _ in vowels)
+    else:
+      return 'This param accepts vowels only'
 
-  if not is_vowel(consonant):
-    lname = ''.join(random.choice(consonant) for _ in consonant)
-  else:
-    print('This param accepts consonants only')
-    return
+    if not is_vowel(consonant):
+      lname = ''.join(random.choice(consonant) for _ in consonant)
+    else:
+      return'This param accepts consonants only'
 
-  fullname = fname +" "+lname
-  return {
-    'Firstname':fname,
-    'Lastname':lname,
-    'Fullname':fullname
-  }
+    fullname = fname +" "+lname
+    return {
+      'Firstname':fname,
+      'Lastname':lname,
+      'Fullname':fullname
+    }
 
 # for i in range(5):
-print(name_generator('ia','cdsdds'))
+# print(name_generator('iae','afghj'))
 
